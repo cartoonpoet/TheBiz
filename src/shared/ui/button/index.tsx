7d04c7dd10b498603c67de993071c8bc76a8505d
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-10-27 15:52:06 +0900

    feat: 메인 섹션3 - Tab3내 버튼 구현

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {CSSProperties} from "react";
 import style from './style.module.css'
 
 interface Props {
     disabled: boolean
     children: React.ReactNode
+    inlineStyle?: CSSProperties
 }
-const Button = ({disabled, children}: Props) => {
-    return <button disabled={disabled} className={style.button}>{children}</button>
+const Button = ({disabled, children, inlineStyle}: Props) => {
+    return <button disabled={disabled} className={style.button} style={inlineStyle}>{children}</button>
 };
 
 export default Button
--- src/widgets/lastSection/ui/index.tsx
+++ src/widgets/lastSection/ui/index.tsx
@@ -11,7 +11,7 @@ const LastSection = () => {
             <div className={style.title}>메인 섹션3 - Tab3</div>
             <div className={style.describe}>섹션3 내 tab 버튼 클릭 시 해당 섹션으로 이동시켜주세요.</div>
             <div className={style.describe}>또한, 스크롤에 따라 탭의 Active 상태를 바꿔주세요.</div>
-            <div className={style.btn}><Button disabled={false}>button</Button></div>
+            <div className={style.btn}><Button disabled={false} inlineStyle={{backgroundColor: 'transparent' }}>button</Button></div>
         </div>
     )
 };
