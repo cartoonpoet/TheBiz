3425de42ff5cf039b1c4d335c2412556962b44d1
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-10-28 22:59:04 +0900

    chore: InputBox에 공통 상태 값적용

--- src/entities/search/ui/index.tsx
+++ src/entities/search/ui/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import style from './style.module.css'
 import SearchIcon from 'assets/image/icon_search_wh.svg'
+import {useHomePageStore} from 'pages/home/model/useHomePageStore'
 
 const Search = () => {
+    const {keyword, setKeyword} = useHomePageStore();
     return (
         <div className={style.wrapper}>
-            <input type="text" placeholder='검색창 placeholder' className={style.inputBox}/>
+            <input type="text" placeholder='검색창 placeholder' className={style.inputBox} value={keyword} onChange={e => {
+                setKeyword(e.target.value)
+            }}/>
             <button className={style.searchBtn}>
                 <img src={SearchIcon} alt="SearchIcon" className={style.searchIcon}/>검색
             </button>
--- src/pages/home/model/useHomePageStore.ts
+++ src/pages/home/model/useHomePageStore.ts
@@ -2,13 +2,20 @@ import {create} from "zustand";
 import {MenuType} from 'entities/menu/model/model'
 
 interface HomeState {
-    menuList: string[]
+    menuList: string[];
+    keyword: string;
+    setKeyword: (keyword: string) => void;
 }
 
 const init = {
-    menuList: new Array(4).fill('메뉴')
+    menuList: new Array(4).fill('메뉴'),
+    keyword: ''
 }
 
-export const useHomePageStore = create<HomeState>((set) => ({
-    ...init,
-}));
+export const useHomePageStore = create<HomeState>((set) => {
+    return ({
+        ...init,
+        setKeyword: (keyword: string) => set(state => ({keyword}))
+
+    });
+});
